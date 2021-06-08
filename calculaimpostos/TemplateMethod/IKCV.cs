d35ce7f3555dcdd1ea57fd68699c7b83818bdf9f
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2021-06-08 20:45:22 -0300

    Template Method - aula 02

---
+++ calculaimpostos/TemplateMethod/IKCV.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoDesignPatterns.TemplateMethod
+{
+    public class IKCV : TemplateMethod
+    {
+        public override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor > 500 && TemItemMaiorQue100ReaisNo(orcamento);
+        }
+
+        private bool TemItemMaiorQue100ReaisNo(Orcamento orcamento)
+        {
+            foreach (var item in orcamento.Items)
+            {
+                if (item.Valor >100)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.10;
+        }
+
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.06;
+        }
+    }
+}
---
+++ calculaimpostos/TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoDesignPatterns.TemplateMethod
+{
+    public abstract class TemplateMethod : IImposto
+    {
+        public double Calcula(Orcamento orcamento)
+        {
+            if (DeveUsarMaximaTaxacao(orcamento))
+            {
+                return MaximaTaxacao(orcamento);
+            }
+            else
+            {
+                return MinimaTaxacao(orcamento);
+            }
+        }
+
+        public abstract double MaximaTaxacao(Orcamento orcamento);
+        public abstract double MinimaTaxacao(Orcamento orcamento);
+        public abstract bool DeveUsarMaximaTaxacao(Orcamento orcamento);
+    }
+}
