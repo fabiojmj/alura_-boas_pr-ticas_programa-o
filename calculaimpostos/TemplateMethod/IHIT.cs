b3300839555a605067b63e3783ec0f2e7160514b
Fabio Mendonca <fabio.jose.mendonca@gmail.com>
2021-06-10 20:55:05 -0300

    Template Method - aula imposto z

---
+++ calculaimpostos/TemplateMethod/IHIT.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoDesignPatterns.TemplateMethod
+{
+    public class IHIT : TemplateMethod
+    {
+        public override bool DeveUsarMaximaTaxacao(Orcamento orcamento)
+        {
+            return ItemRepetidos(orcamento);
+        }
+
+        private bool ItemRepetidos(Orcamento orcamento)
+        {
+            foreach (var item in orcamento.Items)
+            {
+                foreach (var itemProcura in orcamento.Items)
+                {
+                    if(itemProcura.Nome == item.Nome)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public override double MaximaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.13 + 100;
+        }
+
+        public override double MinimaTaxacao(Orcamento orcamento)
+        {
+            return orcamento.Items.Count * 0.01;
+        }
+    }
+}
